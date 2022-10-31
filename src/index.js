21f261bc6a9aee856632ac21cb0f7a8572437da0
GrayNine <zachzachzachj@gmail.com>
2022-10-30 22:14:18 -0400

    Fix API formatting
    Fields corrected in API.  Login template updated accordingly.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import {getFirestore, doc, setDoc} from "firebase/firestore";
 var json = require("./login.json");
 
 const firebaseApp = initializeApp({
-	apiKey: json.apiKey,
-	authDomain: json.authDomain,
-	projectId: json.projectId,
-	storageBucket: json.storageBucket,
-	messagingSenderId: json.messagingSenderId,
-	appId: json.appId,
+	type: json.type,
+	projectId: json.project_id,
+	privateKeyId: json.private_key_id,
+	privateKey: json.private_key,
+	clientEmail: json.client_email,
+	clientId: json.client_id,
+	authUri: json.auth_uri,
+	tokenUri: json.token_uri,
+	authProviderX509CertUrl: json.auth_provider_x509_cert_url,
+	clientX509CertUrl: json.client_x509_cert_url
 });
 
 console.log("Initializing Firestore");
@@ -21,7 +25,7 @@ const testDoc = doc(firestore, "testCollection/testDocument")
 //write to the document
 console.log("Writing to Firestore");
 const docData = {
-	testfield: "testing"
+	testfield: "testing2"
 };
 setDoc(testDoc, docData)
 console.log("Wrote to Firestore");
